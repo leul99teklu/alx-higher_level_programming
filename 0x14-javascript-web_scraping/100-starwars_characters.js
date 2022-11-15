66e98edb32cebe6f4d8370a5ad98289c2545fa09
leul99teklu <leul99teklu@gmail.com>
2022-11-15 13:04:33 -0500

    [written] task 7 Who was playing in this movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,25 @@
 #!/usr/bin/node
+// prints all characters of a Star Wars movie:
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+const process = require('process');
+const filmId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${filmId}`;
 
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const characters = resp.characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
+request.get(apiUrl, (err, response, body) => {
+  if (err === null) {
+    const film = JSON.parse(body);
+    const characters = film.characters;
+    characters.forEach((character) => {
+      request.get(character, (charError, response, body) => {
+        if (err === null) {
+          const characterInfo = JSON.parse(body);
+          console.log(characterInfo.name);
+        } else {
+          console.log(charError);
         }
       });
-    }
+    });
+  } else {
+    console.log(err);
   }
 });
